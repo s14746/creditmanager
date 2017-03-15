60600f7f00c938351386f2e9fedfc7860487a5c6
marlena.franckiewicz <s14746@pjwstk.edu.pl>
2017-03-15 20:47:12 +0100

    Dodano kalkulację dla rat malejących.

--- src/main/java/Kalkulator.java
+++ src/main/java/Kalkulator.java
@@ -4,6 +4,38 @@ import java.util.List;
 public class Kalkulator {
 
     public List<RataKredytu> calculate(Formularz formularz) {
+        if (formularz.typRaty == TypRaty.STALA) {
+            return wyliczRatyStale(formularz);
+        }
+
+        return wyliczRatyMalejace(formularz);
+    }
+
+    private List<RataKredytu> wyliczRatyMalejace(Formularz formularz) {
+        List<RataKredytu> rataKredytuList = new ArrayList<>();
+
+        double poprzeniaKwotaKapitalowa = 0;
+
+        for (int i = 1; i <= formularz.iloscRat; i++) {
+            double kwotaKapitalowa = formularz.kwotaKredytu / formularz.iloscRat;
+            double kwotaOdsetkowa = (formularz.kwotaKredytu - poprzeniaKwotaKapitalowa) * ((formularz.oprocentowanie / 100) / (formularz.iloscRat / 12));
+            double rata = kwotaKapitalowa + kwotaOdsetkowa;
+
+            poprzeniaKwotaKapitalowa = kwotaKapitalowa;
+
+            RataKredytu rataKredytu = new RataKredytu();
+            rataKredytu.numerRaty = i;
+            rataKredytu.kwotaKapitalu = kwotaKapitalowa;
+            rataKredytu.kwotaOdsetek = kwotaOdsetkowa;
+            rataKredytu.oplatyStale = formularz.oplataStala;
+            rataKredytu.calkowitaKwotaRaty = rata;
+            rataKredytuList.add(rataKredytu);
+        }
+
+        return rataKredytuList;
+    }
+
+    private List<RataKredytu> wyliczRatyStale(Formularz formularz) {
         List<RataKredytu> rataKredytuList = new ArrayList<RataKredytu>();
 
         for (int i = 1; i <= formularz.iloscRat; i++) {
--- src/test/java/KalkulatorTest.java
+++ src/test/java/KalkulatorTest.java
@@ -31,4 +31,36 @@ public class KalkulatorTest {
         assertEquals(rataKredytu1.oplatyStale, 20, 2);
         assertEquals(rataKredytu1.calkowitaKwotaRaty, 125, 2);
     }
+
+
+    @Test
+    public void kwota100000IloscRat144Oprocentowanie35OplataStala0TypRatyMalejaca() throws Exception {
+        // given
+        Formularz formularz = new Formularz();
+        formularz.kwotaKredytu = 100000;
+        formularz.iloscRat = 144;
+        formularz.oprocentowanie = 3.5;
+        formularz.oplataStala = 0;
+        formularz.typRaty = TypRaty.MALEJACA;
+
+        // when
+        List<RataKredytu> ratyKredytu = kalkulator.calculate(formularz);
+
+        // then
+        assertEquals(144, ratyKredytu.size());
+
+        RataKredytu rataKredytu1 = ratyKredytu.get(0);
+        assertEquals(rataKredytu1.numerRaty, 1);
+        assertEquals(rataKredytu1.kwotaKapitalu, 694.44, 2);
+        assertEquals(rataKredytu1.kwotaOdsetek, 291.67, 2);
+        assertEquals(rataKredytu1.oplatyStale, 0, 2);
+        assertEquals(rataKredytu1.calkowitaKwotaRaty, 986.11, 2);
+
+        RataKredytu rataKredytu2 = ratyKredytu.get(1);
+        assertEquals(rataKredytu2.numerRaty, 2);
+        assertEquals(rataKredytu2.kwotaKapitalu, 694.44, 2);
+        assertEquals(rataKredytu2.kwotaOdsetek, 289.64, 2);
+        assertEquals(rataKredytu2.oplatyStale, 0, 2);
+        assertEquals(rataKredytu2.calkowitaKwotaRaty, 984.08, 2);
+    }
 }
